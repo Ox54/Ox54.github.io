837c8778faa4206a1831ab39cad8a112a7831748
0x54 <taylor@sondr.co>
2026-04-15 18:34:10 -0400

    actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import remarkGfm from 'remark-gfm';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://ox54.github.io',
 	integrations: [
 		mdx({
 			remarkPlugins: [remarkGfm],
